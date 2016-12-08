3553a63a3d1c5666b6ec90c114c5bbf48174dcb9
poppabear8883 <servnx@gmail.com>
2016-12-08 18:10:26 -0500

    Added bids() relationship to User Model

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function bids() {
+        return $this->hasMany(Bid::class);
+    }
+
 }
